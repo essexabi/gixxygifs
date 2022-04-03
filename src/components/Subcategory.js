b5b3658e36da72861ac1c9b60e26fff7cbe073e8
xabi <essexabi@gmail.com>
2022-04-03 17:07:35 +0200

    Subcategory added window scroll

--- src/components/Subcategory.js
+++ src/components/Subcategory.js
@@ -8,7 +8,7 @@ export default function Subcategory(props) {
     
     return (
         <Link className="Category-link" to={`/search/${props.name}`} key="pprops.name">
-            <a>
+            <a onClick={window.scrollTo(0, 0)}>
                 <div
                     className="Category-item"
                     style={{ "--url": "url('"+ gif[0].url+"')" }}
